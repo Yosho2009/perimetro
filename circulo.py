f1be27fe76559325bd20c93bbd88218782a19ef4
Yosho2009 <alonsozaid294@gmail.com>
2025-12-19 12:32:31 -0600

    hice el perimetro de circolo zaid

---
+++ circulo.py
@@ -0,0 +1,6 @@
+def perimetro():
+    print("Calculando el perímetro del círculo")
+    radio = float(input("Ingrese el radio del círculo: "))
+    perimetro = 2 * 3.1416 * radio
+    print(f"El perímetro del círculo es: {perimetro}")
+()
