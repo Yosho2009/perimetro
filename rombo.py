4c19ff8de2b4560cdbed42041887532916d3c665
Yosho2009 <alonsozaid294@gmail.com>
2025-12-19 12:51:08 -0600

    cree perimetro de rombo israel

---
+++ rombo.py
@@ -0,0 +1,6 @@
+def perimetro():
+    print("Calculando el perimetro del rombo ")
+    l = int(input("Ingresa el numero de un lado : "))
+    r = l + l + l + l 
+    print("El perimetro del rombo es :", r)
+()
